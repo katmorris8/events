1bd63cfe2ebec84ac329397596f054acc8ad0083
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-12-14 18:30:00 -0500

    adjust the event capture

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function logText(e) {
   console.log(this.classList.value);
 }
 
+divs.forEach(div => div.addEventListener('click', logText, { capture: false }))
+
+// event capture/stack
+// captures from the top down nested elements
+// if options object container capture: true, the events are fired from the top down
+// meaning they don't bubble up
+// false by default
+
+// event bubble starts at the top of the stack or the bottom of the nested elements
